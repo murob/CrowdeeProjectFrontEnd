1567c74c4259ebd2e820b7d66574d9baab92a479
seong <59963039+seong7517@users.noreply.github.com>
2021-08-23 18:18:08 +0900

    펀딩 등록 그리드 및 로직
    펀딩 등록 페이지

---
+++ src/views/Components/Write/WriteDefault.js
@@ -0,0 +1,68 @@
+import { Container } from '@material-ui/core';
+import SimpleSelect from 'components/CrowdeeComponents/SimpleSelect';
+import React from 'react';
+import { Form } from 'react-bootstrap';
+import Write from 'views/Components/Editor/Write';
+import TextField from '@material-ui/core/TextField';
+import CustomInput from 'components/CustomInput/CustomInput';
+export default function WriteDefault() {
+    return (
+        <div style={{paddingTop:'30px', paddingBottom:'30px', backgroundColor:'#FCFCFC',}}>
+                <Container maxWidth="md">
+                    <div style={{borderBottom:'1px solid #CECECE', paddingBottom:'40px', marginBottom:'40px', display:'flex', justifyContent:'space-between'}}>
+                        <div>
+                            <h4 style={{fontWeight:'bold'}}>프로젝트 카테고리</h4>
+                            <h6>프로젝트 성격상 가장 일치하는 카테고리를 선택해주세요.</h6>
+                        </div>
+                        <div style={{display:'flex', alignItems:'center'}}>
+                            <SimpleSelect/>
+                        </div>
+                    </div>
+
+                    <div style={{borderBottom:'1px solid #CECECE', paddingBottom:'40px', marginBottom:'40px', display:'flex', justifyContent:'space-between'}}>
+                        <div>
+                            <h4 style={{fontWeight:'bold'}}>프로젝트 제목</h4>
+                            <h6>프로젝트의 주제, 창작물의 특징이 드러나는 멋진 제목을 붙여주세요.</h6>
+                        </div>
+                        <div>
+                            <h5 style={{fontWeight:'bold'}}>긴 제목</h5>
+                            <input style={{border:'1px solid #B6B7B9', borderRadius:'3px', width:'450px', height:'40px'}} placeholder={'긴 제목을 입력해주세요'}></input>
+                            <h5 style={{fontWeight:'bold'}}>짧은 제목</h5>
+                            <input style={{border:'1px solid #B6B7B9', borderRadius:'3px', width:'450px', height:'40px'}} placeholder={'짧은 제목을 입력해주세요'}></input>
+                        </div>
+                    </div>
+
+                    <div style={{borderBottom:'1px solid #CECECE', paddingBottom:'40px', marginBottom:'40px', display:'flex', justifyContent:'space-between'}}>
+                        <div>
+                            <h4 style={{fontWeight:'bold'}}>프로젝트 요약</h4>
+                            <h6>후원자 분들이 프로젝트를 빠르게 이해할 수 있도록<br/>명확하고 간략하게 소개해주세요.</h6>
+                        </div>
+                        <div>
+                            <input style={{border:'1px solid #B6B7B9', borderRadius:'3px', width:'450px', height:'100px'}} placeholder={'프로젝트에 대해 요약해서 입력해주세요'}></input>
+                        </div>
+                    </div>
+
+                    <div style={{borderBottom:'1px solid #CECECE', paddingBottom:'40px', marginBottom:'40px', display:'flex', justifyContent:'space-between'}}>
+                        <div>
+                            <h4 style={{fontWeight:'bold'}}>프로젝트 대표이미지</h4>
+                            <h6>후원자들이 프로젝트의 내용을 쉽게 파악하고<br/>좋은 인상을 받을 수 있도록 이미지 가이드라인을 따라주세요.</h6>
+                        </div>
+                        <div style={{display:'flex', alignItems:'center'}}>
+                            <input type="file"></input>
+                        </div>
+                    </div>
+
+                    <div style={{borderBottom:'1px solid #CECECE', paddingBottom:'40px', marginBottom:'40px', display:'flex', justifyContent:'space-between'}}>
+                        <div>
+                            <h4 style={{fontWeight:'bold'}}>검색태그</h4>
+                            <h6>잠재 후원자의 관심사를 고려한 검색 태그를 입력해주세요.</h6>
+                        </div>
+                        <div>
+                            <input style={{border:'1px solid #B6B7B9', borderRadius:'3px', width:'450px', height:'80px'}} placeholder={'예시) 피아노,사극,현대무용'}></input>
+                        </div>
+                    </div>
+
+                </Container>    
+            </div> 
+    );
+};
