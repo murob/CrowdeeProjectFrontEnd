3cb191cf6a558e1780b1cc810113a24f0526d163
moon <bwdnr95@gmail.com>
2021-08-27 16:10:47 +0900

    projectStartPage 테스트케이스 확인 후  기능 추가 완료

--- src/views/WritePage/ProjectStartPage.js
+++ src/views/WritePage/ProjectStartPage.js
@@ -9,7 +9,8 @@ import FeedbackIcon from '@material-ui/icons/Feedback';
 import { Button } from '@material-ui/core';
 import { request } from "Utils/Utils";
 import { Link, Switch } from 'react-router-dom';
-
+import CustomInput from "components/CustomInput/CustomInput.js";
+import { createTrue } from 'typescript';
 const useStyles = makeStyles(styles);
 
 export default function ProjectStartPage(props) {
@@ -17,27 +18,32 @@ export default function ProjectStartPage(props) {
     const { ...rest } = props;
     const [manageUrl,setManageUrl] = useState()
     const [projectUrl,setProjectUrl] = useState({projectUrl:""})
-
+    const [urlCheck,setUrlCheck] = useState(false)
+    const [urlText,setUrlText] = useState("www.Crowdee.com/고객님의 URL")
+    const [confirm , setConfirm] = useState(false)
+    const [start , setStart] = useState('none')
     const urlChange = (e) => {
         setProjectUrl(
             e.target.value
         );
-        console.log(projectUrl)
+        setStart('none')
+        setConfirm(false)
+        setUrlText("www.Crowdee.com/고객님의 URL")
     }
 
     const doubleCheck = () => {
         console.log('중복확인')
-        fetch(`http://localhost:8081/creator/checkUrl/${projectUrl}`, {
-            headers:{
-              
+        fetch(`http://localhost:8081/creator/checkUrl/${projectUrl}`).
+        then((res)=>{
+            if(res.status==400||res.status==500){
+                setUrlText("URL이 중복됩니다.")
+                setUrlCheck(true)
             }
-        }).then((res)=>{
-            if(res.status==200){
-                console.log('중복URL 없음')
-                submitStart()
-            }
-            else if(res.status==400||res.status==401||res.status==403||res.status==404){
-                console.log('URL이 중복됩니다.')
+            else {
+                setUrlText('사용가능한 URL입니다.')
+                setUrlCheck(false)
+                setStart('flex')
+                setConfirm(true)
             }
         })
     }
@@ -56,15 +62,14 @@ export default function ProjectStartPage(props) {
             res.json();
         }).
         then((res)=>{
-            setManageUrl(res.tempFundingDTO.manageUrl)
-            props.history.push(`/write-page/${manageUrl}`)
+            if(confirm){
+                setManageUrl(res.tempFundingDTO.manageUrl)
+                props.history.push(`/write-page/${manageUrl}`)
+            }
+            
         })
     }
-    const subStart = () => {
-        console.log('default페이지로 이동')       
-        props.history.push("/write-page")
-        
-    }
+    
     return (
         <div style={{backgroundColor:'white'}}>
             <Header
@@ -98,11 +103,28 @@ export default function ProjectStartPage(props) {
                             <h6>프로젝트 페이지로 접속하기 위한 웹페이지 주소(URL)를 설정 해주세요.</h6>
                         </div>
                         <div>
-                            <input type="text" name="projectUrl" onChange={urlChange} style={{border:'1px solid #EDEBEB', borderRadius:'3px', width:'200px', height:'40px'}} placeholder={'URL을 입력해주세요'}></input>
-                            <Button variant="contained" style={{marginLeft:'10px'}} onClick={doubleCheck}>중복확인</Button>
+                        <CustomInput
+                      labelText={urlText}
+                      error ={urlCheck}
+                      id="url"
+                      underlineClasses={classes.underlineSuccess}
+                      formControlProps={{
+                        fullWidth: false,
+                        style :{hidden:"true",width:"300px"}
+                      }}
+                      inputProps={{
+                        type: "string",
+                        
+                        onChange: urlChange,
+                       
+                        autoComplete: "off",
+                      }}
+                    />
+                            {/* <input type="text" name="projectUrl" onChange={urlChange} style={{border:'1px solid #EDEBEB', borderRadius:'3px', width:'200px', height:'40px'}} placeholder={'URL을 입력해주세요'}></input> */}
+                            <Button variant="contained" style={{marginLeft:'10px', marginTop:"20px"}} onClick={doubleCheck}>중복확인</Button>
                         </div>
                         <div style={{display:'flex', justifyContent:'', paddingTop:'50px'}}>
-                            <Button style={{fontWeight:'bold'}} variant="contained" color="secondary" onClick={submitStart}>시작하기</Button>
+                            <Button style={{fontWeight:'bold',display:`${start}`}} variant="contained" color="secondary" onClick={submitStart}>시작하기</Button>
                         </div>
                     </div>
                 </div>
