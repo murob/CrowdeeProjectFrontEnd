9c1769570956597fedab85a6c3c8c105566b93c4
moon <bwdnr95@gmail.com>
2021-09-01 02:04:18 +0900

    commit

--- src/views/LandingPage/Sections/ProductSection.js
+++ src/views/LandingPage/Sections/ProductSection.js
@@ -83,32 +83,6 @@ export default function ProductSection() {
                 )
                
               )} 
-              {/* {fundings.new.map((funding) => (
-                <>
-                 <GridContainer justify="center">
-                  <GridItem xs={12} sm={12} md={8}>
-                  <Typography>신규 추천 펀딩</Typography>
-                </GridItem>
-                 </GridContainer>
-            
-                <GridContainer justify="center">
-                  <GridItem>
-                    <GridContainer justify="center">
-                      <FundingCard 
-                        id={funding.funding_id}
-                        title={funding.title}
-                        imgUrl={funding.thumbNailUrl}
-                        summary={funding.summary}
-                        restDate={funding.restDate}
-                        category={funding.category}
-                        totalFundraising={funding.totalFundraising}
-                        goalFundraising={funding.goalFundraising}
-                        ROA={funding.rateOfAchievement} />
-                   </GridContainer>
-                  </GridItem>
-                </GridContainer>
-                </>
-              ))}  */}
              
       </div>
     </div>
